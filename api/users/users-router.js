290d96ac8102ba1a9b9b890dc45c6a87b145cd28
Rlauckern <r.lauckern@gmail.com>
2022-04-13 23:12:56 +0000

    updating users-router

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -5,23 +5,32 @@ const {
   validatePost,
 } = require('../middleware/middleware');
 
+const User = require('./users-model');
+const Post = require('../posts/posts-model');
+
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+  User.get()
+  .then(users => {
+    res.json(users)
+  })
+  .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+  res.json(req.user)
 });
 
 router.post('/', validateUser, (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+  User.insert({ name: req.name })
+  .then(newUser => {
+    res.status(201).json(newUser)
+  })
+  .catch(next)
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res) => {
